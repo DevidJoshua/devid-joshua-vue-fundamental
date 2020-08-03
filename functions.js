f249b466a230637312df3d613823e1cde99e6f1b
DevidJoshua <28devid@gmail.com>
2020-08-03 10:34:44 +0700

    App ver. 1

--- functions.js
+++ functions.js
@@ -1,2 +1,24 @@
-const a='coba'
-console.log("ta hubung")
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         message: 'Hello Vue!'
+//     },
+//     //Method-methodny daari functions.js
+//     methods:{
+//         callAPI:function(){
+//             console.log("nilainya message",this.message)
+//         },
+//     }
+// })
+const api = (mes) =>
+{
+    // fetch(`https://cari-teks-video-api.vercel.app/api/search?url=${this.url}&q`)
+    // .then(res=>{
+    //     console.log("hasil==>>",res)
+    // })
+    // .catch(err=>{
+    //     console.log("error==>>",err)
+    // })
+
+}
+
